353fe37309183b539e1467092f125ffbb8457960
Doridian <git@doridian.net>
2022-01-11 15:12:53 -0800

    Allow config via env

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from os import getenv
 from rflib import RfCat
 from registry import DEVICE_TYPES
 from paho.mqtt import client as mqtt_client
@@ -8,11 +9,21 @@ from json import loads as json_loads
 from random import randint
 
 def load_config():
-    global CONFIG
+    global _CONFIG
     fh = open("config.yml", "r")
-    CONFIG = yaml_load(fh)
+    _CONFIG = yaml_load(fh)
     fh.close()
-load_config()
+
+def config_get(key, default=None):
+    env = getenv(f"RFCAT_MQTT_{key.upper()}")
+    if env is not None:
+        return env
+    if key not in _CONFIG:
+        return default
+    return _CONFIG[key]
+
+def config_get_int(key, default=None):
+    return int(config_get(key, default))
 
 client_id = f"rfcat-mqtt-{randint(0, 1000)}"
 
@@ -24,9 +35,9 @@ def connect_mqtt():
             print("Failed to connect, return code", rc)
             exit(1)
     client = mqtt_client.Client(client_id)
-    client.username_pw_set(CONFIG["username"], CONFIG["password"])
+    client.username_pw_set(config_get("username"), config_get("password"))
     client.on_connect = on_connect
-    client.connect(CONFIG["broker"], CONFIG["port"])
+    client.connect(config_get("broker"), config_get_int("port"))
     return client
 
 def on_message(client, userdata, msg):
@@ -45,8 +56,10 @@ def run():
     RFCAT_DEV = RfCat()
     RFCAT_DEV.ping()
 
+    load_config()
+
     client = connect_mqtt()
-    client.subscribe(CONFIG["topic"])
+    client.subscribe(config_get("topic"))
     client.on_message = on_message
     client.loop_forever()
 
