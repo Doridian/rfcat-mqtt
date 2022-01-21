f670a9c5fdbfff7ad105c26a5ff5fc2ee79a0cce
Doridian <git@doridian.net>
2022-01-20 16:58:23 -0800

    force exit

---
+++ utils.py
@@ -0,0 +1,20 @@
+from threading import Timer
+
+def do_every(interval, worker_func, iterations = 0):
+  if iterations != 1:
+    Timer (
+      interval,
+      do_every, [interval, worker_func, 0 if iterations == 0 else iterations-1]
+    ).start()
+
+  worker_func()
+
+def ping_every(interval, dev):
+    def ping_func():
+        try:
+            dev.ping()
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    do_every(interval, ping_func)
